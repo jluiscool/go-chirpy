8ff41957426c407c891870438468cb52f3095add
Jose A <jfacademusic@gmail.com>
2023-11-30 12:08:53 -0500

    added fs and index html

--- main.go
+++ main.go
@@ -6,29 +6,29 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 	//create new server multiplexer
 	mux := http.NewServeMux()
+	//make mux handle request to root path
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	//run it through middleware for Cors header change
 	corsMux := middlewareCors(mux)
 
-	//create server
+	//create server from a struct that describes the server configuration
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
+	//logs server closed
 	log.Fatal(srv.ListenAndServe())
-
-	// // assign it as http handler for root
-	// http.Handle("/", corsMux)
-	// //port to work on
-	// http.ListenAndServe(":8080", nil)
 }
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//Headers need to be modified so other websites can send request.
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")

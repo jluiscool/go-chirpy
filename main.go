9d7ee9a0985665e877c34147c8c93114eb30ebfa
Jose A <jfacademusic@gmail.com>
2023-12-01 16:28:20 -0500

    added strip prefix

--- main.go
+++ main.go
@@ -11,18 +11,19 @@ func main() {
 	//create new server multiplexer
 	mux := http.NewServeMux()
 	//make mux handle request to root path
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	//allows the fileserver to serve index.html without returning index.html
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", readynessEndPoint)
 	//run it through middleware for Cors header change
 	corsMux := middlewareCors(mux)
-
 	//create server from a struct that describes the server configuration
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
-
 	log.Printf("Serving on port: %s\n", port)
-	//logs server closed
+	//ListenAndServe listens to TCP server.Addr, then calls Serve to handle incoming requests
+	//main function blocks until the server is shut down, returning an error
 	log.Fatal(srv.ListenAndServe())
 }
 
@@ -38,4 +39,12 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func readynessEndPoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	body := "this is the body"
+	byteSlice := []byte(body)
+	w.Write(byteSlice)
 }
